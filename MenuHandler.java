71a74af32097127351771bf820f73359c7e97e8a
danielccorrea21 <danielc.correa21@gmail.com>
2016-11-29 18:39:39 -0200

    Tela nova pergunta

---
+++ MenuHandler.java
@@ -0,0 +1,90 @@
+package controller;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import model.tiposAcesso;
+import view.ItemMenu;
+import view.TelaLogin;
+import view.TelaMenu;
+import view.TelaNovaPergunta;
+import view.TelaRanking;
+
+public class MenuHandler {
+	private TelaMenu menuView;
+	private tiposAcesso tipoAcesso;
+	
+	public MenuHandler(TelaMenu menuView, tiposAcesso tipoAcesso) {
+		this.menuView = menuView;
+		this.tipoAcesso = tipoAcesso;
+		
+		this.menuView.addMenuListener(new MenuListener());
+		
+		setTipoMenu();
+		menuView.pack();
+	}
+
+	private void setTipoMenu() {
+		ItemMenu[] opcoes = menuView.getOpcoes();
+		
+		switch (tipoAcesso) {
+			case ALUNO:
+		        opcoes[1].setBotaoOpcaoText("Minhas Perguntas");
+		        opcoes[1].setBotaoOpcaoName("MINHAS_PERG");
+		        menuView.unhideItem();
+		        break;
+			case PROFESSOR:
+		        opcoes[1].setBotaoOpcaoText("Responder Perguntas");
+		        opcoes[1].setBotaoOpcaoName("RESP_PERG");
+		        menuView.hideItem();
+		        break;
+		}
+		
+		menuView.setOpcoes(opcoes);
+	}
+	
+	class MenuListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent evt) {
+			Component component = (Component) evt.getSource();
+			switch (component.getName()) {
+
+			case "CRIAR_PERG":
+				TelaNovaPergunta telanovapergunta = new TelaNovaPergunta();
+				telanovapergunta.setVisible(true);
+				telanovapergunta.setLocationRelativeTo(null);
+				
+				break;
+
+			case "MINHAS_PERG":
+				break;
+
+			case "RESP_PERG":
+				break;
+
+			case "PESQ_PERG":
+				break;
+				
+			case "RANKING":
+				TelaRanking telaRanking = new TelaRanking();
+				new RankingHandler(telaRanking); 
+				telaRanking.setVisible(true);
+				telaRanking.setLocationRelativeTo(null);
+				break;
+
+			case "LOGOUT":
+				TelaLogin telaLogin = new TelaLogin();
+				new LoginHandler(telaLogin); 
+				telaLogin.setVisible(true);
+				telaLogin.setLocationRelativeTo(null);
+				menuView.dispose();
+				break;
+				
+			case "SAIR":
+				System.exit(0);
+			}
+		}
+	}
+}
